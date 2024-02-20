01be3641a88e5a37be5bcdb14f5e24869e655603
Meet Patel <98462969+meet4592@users.noreply.github.com>
2024-02-21 00:00:41 +0530

    Update Student.js

--- src/models/Student.js
+++ src/models/Student.js
@@ -1,24 +1,17 @@
-// models/Student.js
+import mongoose from "mongoose";
 
-import { DataTypes } from 'sequelize';
-import { sequelize } from '../utils/db'; // Assuming you export sequelize from connectToDatabase function
+const { Schema } = mongoose;
 
-const Student = sequelize.define('Student', {
-  rollnumber: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  marks: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-}, {
-  tableName: 'students', // Assuming your table name in the database is 'students'
-});
 
-export default Student;
+const studentSchema = new Schema({
+    rollnumber: {
+        type: String,
+      
+    },
+    name: {
+        type: String,
+    }
+})
+
+const Student = model('students', studentSchema)
+export default StudentModel
